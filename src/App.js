09d2d3020bd5a9d1050b1bf6c460d0f78dbaad82
Guillermo González <ggonzalezh15@gmail.com>
2020-02-11 23:46:43 -0300

    Fix mobile version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
-import React from 'react';
-import construction from './multimedia/me.jpg';
+import React, { Component } from 'react';
+import Cloud from './components/cloud-text/cloudText';
 import './App.css';
 import Particles from 'react-particles-js';
-import particlesOpts from './components/particles';
-import twitterImage from './multimedia/twitter.png';
-import githubImage from './multimedia/github.svg';
-import gmailImage from './multimedia/gmail.png';
 
-function App() {
-  return (
-    <div>
-      <div className="Content">
-        <div className="wrapper">
-          <a href="https://github.com/ggonzalezh">
-            <div className="quarter left-top">
-              <img src={githubImage} className="githubIcon" alt="gitHubImage" />
-            </div>
-          </a>
-          <a href="mailto:hello@ggonzalezh.dev">
-            <div className="quarter right-top">
-              <img src={gmailImage} className="gmailIcon" alt="gmailImage" />
-            </div>
-          </a>
-          <a href="https://twitter.com/ggonzalezh_cl">
-            <div className="quarter left-bottom">
-              <img src={twitterImage} className="twitterIcon" alt="twitterImage" />
-            </div>
-          </a>
-          <div className="quarter right-bottom">
-            <img src={construction} className="image" alt="me" />
-          </div>
-        </div>
-        <br></br>
-        <div className="speech-bubble">
-          <div className="bubble-margin">
-            <h2 className="say-hi">Hola,</h2>
-            <div className="presentation-all-text">
-              <p>Mi nombre es Guillermo González, soy desarrollador de software y radico en Santiago, Chile.</p>
-              <p>Me gusta JavaScript, TypeScript, los proyectos de comunidad y el continuo aprendizaje.</p>
-              <p>Me considero una persona tranquila, pero ante cualquier conflicto, nos vemos en el Smash Bros junto a unas cervezas.</p>
-              <p>Si te quieres comunicar conmigo, te invito a enviar un mail a hello@ggonzalezh.dev</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Particles className="Background"
-        params={particlesOpts}
-      />
-    </div>
-  );
+const particlesOpts = {
+  "particles": {
+      "number": {
+          "value": 200,
+          "density": {
+              "enable": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+              "speed": 4,
+              "size_min": 0.3
+          }
+      },
+      "line_linked": {
+          "enable": false
+      },
+      "move": {
+          "random": true,
+          "speed": 1,
+          "direction": "top",
+          "out_mode": "out"
+      }
+  }
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="Content">
+          <Cloud></Cloud>
+        </div>
+        <Particles className="Background"
+          params={particlesOpts}
+        />
+      </div>
+    )
+  }
+}
--- src/components/svg.js
+++
@@ -1,5 +0,0 @@
-const svgElement = () => {
-    <svg width="200" height="200" viewBox="-2 -2 202 203" shape-rendering="geometricPrecision">
-        <p>asd</p>
-    </svg >
-};
