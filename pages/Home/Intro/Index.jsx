d21b5c441e89601bda204f13a59199f9c0b13b2f
ggonzalezh <ggonzalezh15@gmail.com>
2020-12-06 01:40:03 -0300

    monkas

---
+++ pages/Home/Intro/Index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import IntroContainer from "./Style";
+const Intro = () => {
+  return (
+    <IntroContainer>
+      <h3>Hi, I am Guillermo</h3>
+      <p>
+        A full stack developer who loves open source and values learning and
+        growing with people, teams, and technologies.
+      </p>
+      <p>Read more about me or contact me</p>
+    </IntroContainer>
+  );
+};
+
+export default Intro;
