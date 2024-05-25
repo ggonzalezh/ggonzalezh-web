f8c09e2ca54035c8f83d84279ea50586f3335b5b
ggonzalezh <ggonzalezh15@gmail.com>
2024-05-25 04:30:47 -0400

    :construction: Upgrade to Next 14 and added Coming soon text

--- components/Footer/Style.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components'
-
-const LayoutContainer = styled.div``
-
-export default LayoutContainer
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import './page.css'
+
+export default function Home() {
+  return (
+    <main className="flex h-screen justify-center items-center">
+      <div className='typewriter'>
+          <h1>Coming soon...</h1>
+      </div>
+    </main>
+  );
+}
