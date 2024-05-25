7c890004ec4ed1dfb1e13329ee4eafcba1d319e0
ggonzalezh <ggonzalezh15@gmail.com>
2024-05-25 04:51:13 -0400

    :construction: Change background & meta tag's

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Guillermo González",
-  description: "Guillermo González's personal website",
+  title: "Guillermo González - Software Engineer",
+  description: "Hola! soy Guillermo González, un Software Engineer apasionado por la tecnología y la programación.",
 };
 
 export default function RootLayout({
